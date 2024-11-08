78bf7a13bb540bc27027c207671814740c5b2785
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-11-08 14:00:30 +0530

    checking commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+import controller.BankController;
+import exception.BankingException;
+import exception.InvalidAccountTypeException;
 
 public class Main {
-	
+	public static void main(String[] args) throws NumberFormatException, IOException, SQLException, InvalidAccountTypeException, BankingException
+	{
+		BankController bc =new BankController();
+		bc.start();
+	}
 }
